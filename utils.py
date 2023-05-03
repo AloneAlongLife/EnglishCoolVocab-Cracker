b5e1ad1042f29ebcb6b9f0b3b775ec7e13c55af8
CHIH-HAO CHUANG <xd0931021@gmail.com>
2023-05-03 17:20:17 +0800

    Commit

--- utils.py
+++ utils.py
@@ -34,7 +34,9 @@ def update_data():
     run(f"adb -s {HOST_NAME} shell mv /sdcard/Download/wordcool_user.db /storage/emulated/0/Android/data/com.EnglishCool.Vocab/files/wordcool_user.db")
 
 def get_f_data():
-    run(f"adb -s {HOST_NAME} pull /data/data/com.EnglishCool.Vocab/shared_prefs/com.EnglishCool.Vocab.v2.playerprefs.xml")
+    run(f"adb -s {HOST_NAME} shell su -c cp /data/data/com.EnglishCool.Vocab/shared_prefs/com.EnglishCool.Vocab.v2.playerprefs.xml /sdcard/Download/com.EnglishCool.Vocab.v2.playerprefs.xml")
+    run(f"adb -s {HOST_NAME} pull /sdcard/Download/com.EnglishCool.Vocab.v2.playerprefs.xml")
+    run(f"adb -s {HOST_NAME} shell su -c rm /sdcard/Download/com.EnglishCool.Vocab.v2.playerprefs.xml")
 
 def update_f_data():
     run(f"adb -s {HOST_NAME} push com.EnglishCool.Vocab.v2.playerprefs.xml /sdcard/Download/")
